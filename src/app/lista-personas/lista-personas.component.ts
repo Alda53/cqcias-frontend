3827c7d4e2b5564535d733bbfdd9d6bea5e71f17
Alda53 <caldair99@gmail.com>
2024-02-01 08:10:51 -0600

    sorted table

--- src/app/lista-personas/lista-personas.component.ts
+++ src/app/lista-personas/lista-personas.component.ts
@@ -20,6 +20,38 @@ export class ListaPersonasComponent implements OnInit{
 
   constructor(private personaServicio:PersonaService, private modalSS:ServiceSwitchService){}
 
+  criterioOrden: string;
+  ordenAscendente = true;
+
+  // Método para cambiar el criterio de orden
+  cambiarCriterioOrden(criterio: string): void {
+    if (this.criterioOrden === criterio) {
+      // Si hacemos clic en el mismo criterio, invertimos el orden
+      this.ordenAscendente = !this.ordenAscendente;
+    } else {
+      // Si cambiamos a un nuevo criterio, lo establecemos y orden ascendente por defecto
+      this.criterioOrden = criterio;
+      this.ordenAscendente = true;
+    }
+  }
+
+  // Método para aplicar la clasificación
+  aplicarClasificacion(personas: any[]): any[] {
+    if (!this.criterioOrden) {
+      // Si no hay criterio de orden, devolvemos el arreglo sin cambios
+      return personas;
+    }
+
+    // Aplicamos la clasificación
+    return personas.sort((a, b) => {
+      const valorA = a[this.criterioOrden];
+      const valorB = b[this.criterioOrden];
+
+      // Operador ternario para determinar el orden ascendente o descendente
+      return this.ordenAscendente ? valorA.localeCompare(valorB) : valorB.localeCompare(valorA);
+    });
+  }
+
   ngOnInit(): void {
     this.obtenerPersonas();
     this.modalSS.$modal.subscribe((value)=>{this.modalSwitch=value})
